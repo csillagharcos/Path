2cd0942a2a074546ecf29ad951fded909f776f6a
Tusky <tuskone16@gmail.com>
2012-09-09 02:29:09 +0200

    csv import for c1 form

--- report_forms/c1/forms.py
+++ report_forms/c1/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 from report_forms.c1.models import c1
 
 class C1Form(ModelForm):
@@ -6,3 +6,5 @@ class C1Form(ModelForm):
         model = c1
         exclude = ('added_by')
 
+class FileUploadForm(forms.Form):
+    file  = forms.FileField()
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 def rel(*x):
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
