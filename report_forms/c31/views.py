fd3a58b9e5dec97c8230e7fabc8b77f02b2366cc
Tusky <tuskone16@gmail.com>
2012-09-23 13:37:47 +0200

    c31 import done

--- report_forms/c31/views.py
+++ report_forms/c31/views.py
@@ -44,13 +44,13 @@ def Import(request):
         for line in imported_csv:
             try:
                 new_c31 = c31.objects.create(
-                                            patient_id                      = line.patient_id,
-                                            case_id                         = line.case_id,
+                                            patient_id                      = int(line.patient_id),
+                                            case_id                         = int(line.case_id),
                                             date_of_birth                   = datetime.strptime(line.date_of_birth, "%Y-%m-%d"),
                                             date_of_admission               = datetime.strptime(line.date_of_admission, "%Y-%m-%d"),
-                                            patient_admission_status        = line.patient_admission_status,
+                                            patient_admission_status        = int(line.patient_admission_status),
                                             date_of_discharge               = datetime.strptime(line.date_of_discharge, "%Y-%m-%d"),
-                                            patient_discharge_status        = line.patient_discharge_status,
+                                            patient_discharge_status        = int(line.patient_discharge_status),
                                             icd                             = line.icd,
                                             added_by                        = request.user,
                 )
