a69c09807b6abb85e28333f43f7f4b4684dea8e8
Tusky <tuskone16@gmail.com>
2012-09-24 16:55:20 +0200

    design and sheets added

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^reportforms/', include('report_forms.urls')),
+    url(r'^reportforms/', include('report_forms.urls'), name="report_forms"),
     url(r'^knowledgecenter', 'pathstatic.views.knowledge_center', name="knowledge_center"),
     url(r'^$', 'pathstatic.views.homepage', name="homepage"),
     url(r'^login/', 'pathstatic.views.login_page'),
