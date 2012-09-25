ff96fb0c9b4781b8fb3e1e69b4a016c0e4250427
Tusky <tuskone16@gmail.com>
2012-09-25 15:47:42 +0200

    correction

--- report_forms/c1/forms.py
+++ report_forms/c1/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 class C1Form(ModelForm):
     other_diagnoses   = forms.MultipleChoiceField(choices=[(item.pk, item.name) for item in c1OtherDiagnose.objects.all()], widget=forms.CheckboxSelectMultiple())
     date_of_birth     = forms.DateTimeField(label=_('Date of birth'), widget=forms.TextInput(attrs={'class':'datepicker', 'placeholder':_('[click here]')}))
-    date_of_delivery  = forms.DateTimeField(label=_('Date of delivery'), widget=forms.TextInput(attrs={'class':'datetimepicker', 'placeholder':_('[click here]')}))
+    date_of_delivery  = forms.DateTimeField(label=_('Date and time of delivery'), widget=forms.TextInput(attrs={'class':'datetimepicker', 'placeholder':_('[click here]')}))
     class Meta:
         model = c1
         exclude = ('added_by')
