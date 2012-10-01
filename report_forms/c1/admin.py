b43d15e4038fc155a8169c4991c27b2e2ac4e6df
Tusky <tuskone16@gmail.com>
2012-10-01 13:52:10 +0200

    c1 admin table description

--- report_forms/c1/admin.py
+++ report_forms/c1/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from report_forms.c1.models import c1
+from django.utils.translation import ugettext_lazy as _
 
 class c1Admin(admin.ModelAdmin):
     def get_other_diagnoses(self, obj):
@@ -12,8 +13,7 @@ class c1Admin(admin.ModelAdmin):
             else:
                 namelist = namelist+', '+diag.name
         return namelist
-
-    get_other_diagnoses.short_description = 'Test'
+    get_other_diagnoses.short_description = _('Other diagnoses')
     list_display = ('patient_id', 'case_id', 'date_of_birth', 'date_of_delivery', 'number_of_prev_deliveries', 'number_of_prev_deliveries_by_c', 'the_c_section', 'weight_of_the_newborn', 'mother_illness', 'specify_mother_illness', 'drg_code', 'get_other_diagnoses')
 
 
