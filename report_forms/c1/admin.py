742bce80b22767dc86543095b2e6fd8aa5fdcff2
Tusky <tuskone16@gmail.com>
2012-10-01 13:46:26 +0200

    c1 admin table beautify other diagnose

--- report_forms/c1/admin.py
+++ report_forms/c1/admin.py
@@ -3,7 +3,15 @@ from report_forms.c1.models import c1
 
 class c1Admin(admin.ModelAdmin):
     def get_other_diagnoses(self, obj):
-        return obj.other_diagnoses.all()
+        first = True
+        namelist = ""
+        for diag in obj.other_diagnoses.all():
+            if first:
+                namelist = diag.name
+                first = False
+            else:
+                namelist = namelist+', '+diag.name
+        return namelist
 
     get_other_diagnoses.short_description = 'Test'
     list_display = ('patient_id', 'case_id', 'date_of_birth', 'date_of_delivery', 'number_of_prev_deliveries', 'number_of_prev_deliveries_by_c', 'the_c_section', 'weight_of_the_newborn', 'mother_illness', 'specify_mother_illness', 'drg_code', 'get_other_diagnoses')
