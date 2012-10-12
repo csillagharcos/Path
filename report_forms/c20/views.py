8954094f5433db84fb133bed968780ba56a0900e
Richard Hajdu <tuskone16@gmail.com>
2012-10-12 11:54:01 +0300

    fixed case_id

--- report_forms/c20/views.py
+++ report_forms/c20/views.py
@@ -81,11 +81,11 @@ def Import(request):
                 )
                 new_c20.save()
             except IntegrityError:
-                exists += (line.patient_id,)
+                exists += (line.case_id,)
             except DateException, (inst):
-                date_errors += (line.patient_id,)
+                date_errors += (line.case_id,)
             except:
-                errors += (line.patient_id,)
+                errors += (line.case_id,)
         if exists or errors:
             return render_to_response('c20_error.html', {'exists': exists, 'errors': errors, 'date_errors': date_errors}, context_instance=RequestContext(request))
         return HttpResponseRedirect(reverse('c20_stat'))
