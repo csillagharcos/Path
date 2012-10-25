7347ab28c31cdbf1210b35e2197358534cb56018
Hajdu Richard <tusk@hrch.sote.hu>
2012-10-25 12:06:17 +0200

    error message

--- report_forms/c23/views.py
+++ report_forms/c23/views.py
@@ -146,6 +146,7 @@ def Statistics(request):
     countable_case=uncountable_case=()
     cases = c23.objects.all()
     medicines=()
+    a = b = ""
     for medicine in Medicine.objects.all():
         medicines += (medicine.name,)
     for case in cases:
@@ -182,6 +183,8 @@ def Statistics(request):
         if (first_med_dose == case.first_dose or second_med_dose == case.second_dose or (first_med_dose == case.first_dose and second_med_dose == case.second_dose) or (first_med_doseUnder == case.first_dose and second_med_doseUnder == case.second_dose)) and acceptable:
             indicator_twoa += 1
             indicator_tracker += 1
+        else:
+            a += str(case.case_id)+", "
 
         #indicator two B
         if case.weight_of_patient > 60:
@@ -231,9 +234,9 @@ def Statistics(request):
         else: sd = case.second_dose
         if case.date_of_first_dose == case.date_of_last_dose and case.total_dose_in_24h == fd + sd:
             indicator_nine += 1
+        else:
+            b += str(case.case_id)+", "
 
-
-    print indicator_nine
     ''' Counting '''
     try: one = float(indicator_one) / len(countable_case) * 100
     except: one = 0
@@ -274,6 +277,8 @@ def Statistics(request):
         "eight": eight,
         "nine": nine,
         "ten": ten,
+        "a": a,
+        "b": b,
         }
     return render_to_response('c23_statistics.html', context, context_instance=RequestContext(request))
 
