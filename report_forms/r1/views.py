233e4db09453acfa89a73737739f936edeac113d
Tusky <tuskone16@gmail.com>
2012-11-23 12:50:03 +0100

    r1 trend

--- report_forms/r1/views.py
+++ report_forms/r1/views.py
@@ -7,7 +7,7 @@ from django.db.utils import IntegrityError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-from report_forms.r1.forms import r1Form, FileUploadForm
+from report_forms.r1.forms import r1Form, FileUploadForm, TrendForm
 from report_forms.r1.models import r1, r1CSV
 from report_forms.tools import parseFloat, parseInt, csvDump, DateException, csvExport
 from django.utils.translation import ugettext_lazy as _
@@ -311,7 +311,22 @@ def Statistics(request):
 
 @login_required
 def Trend(request):
-    pass
+    if request.method == "POST":
+        form = TrendForm(request.POST)
+        if form.is_valid():
+            interval_one = CountStatistics(r1.objects.filter(added_by__personel__workplace = request.user.get_profile().workplace, date_of_admission__gte = form.cleaned_data['date1a'], date_of_admission__lte = form.cleaned_data['date1b'] ) )
+            interval_two = CountStatistics(r1.objects.filter(added_by__personel__workplace = request.user.get_profile().workplace, date_of_admission__gte = form.cleaned_data['date2a'], date_of_admission__lte = form.cleaned_data['date2b'] ) )
+            if form.cleaned_data['date3a'] and form.cleaned_data['date3b']:
+                interval_three = CountStatistics(r1.objects.filter(added_by__personel__workplace = request.user.get_profile().workplace, date_of_admission__gte = form.cleaned_data['date3a'], date_of_admission__lte = form.cleaned_data['date3b'] ) )
+            else:
+                interval_three = False
+            return render_to_response('r1_trend_diagram.html', { 'one': interval_one, 'two': interval_two, 'three': interval_three, 'form': form.cleaned_data }, context_instance=RequestContext(request))
+        else:
+            form = TrendForm(request.POST)
+            return render(request, 'r1_trend.html', { 'form': form })
+    else:
+        form = TrendForm()
+        return render(request, 'r1_trend.html', { 'form': form })
 
 def Template(request):
     model = (
